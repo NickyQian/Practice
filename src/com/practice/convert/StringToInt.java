60c499ab94cbcdc57f99c9cdab63142ffdc76bc3
Nicky <leqian@ebay.com>
2012-10-10 17:21:00 +0800

    change project name, add worksapplications's work

---
+++ src/com/practice/convert/StringToInt.java
@@ -0,0 +1,17 @@
+package com.practice.convert;
+
+public class StringToInt {
+
+	public int convert(String input){
+		int result = 0;
+		return result;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
