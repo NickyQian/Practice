d9cb0cb9814afb43381282b52f278a3573bbaca3
Nick Q <nicky_qianlei@163.com>
2013-07-11 13:06:20 +0800

    add annotation example

---
+++ src/com/practice/annotation/Target.java
@@ -0,0 +1,13 @@
+package com.practice.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.ANNOTATION_TYPE)
+public @interface Target {
+    ElementType[] value();
+}
