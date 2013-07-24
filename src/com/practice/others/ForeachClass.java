626dde4616ec0eeabbfc55cb0825975eea9900fb
Nick Q <nicky_qianlei@163.com>
2013-07-24 15:07:47 +0800

    add sth about  instanceof and for( : )

---
+++ src/com/practice/others/ForeachClass.java
@@ -0,0 +1,25 @@
+package com.practice.others;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForeachClass {
+    
+    public static void foreachFun(List<String> testStr){
+        for(String item : testStr) {
+            System.out.println(item + "OK");
+        } 
+    }
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        List<String> testStr = new ArrayList<String>();
+       // 如果testStr为null的话 在foreach循环中会出错！
+        // 事先判断是否为null还是有必要的
+            foreachFun(testStr); 
+        System.out.println("the end");
+    }
+
+}
---
+++ src/com/practice/subclassjudgment/Bsubclass.java
@@ -0,0 +1,13 @@
+package com.practice.subclassjudgment;
+
+public class Bsubclass implements IBasicClass {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
