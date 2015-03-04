2813a27d70f56839b525215060054918a66f6806
Nicky <nicky_qianlei@163.com>
2015-03-04 09:13:05 +0800

    Add Utils.

---
+++ src/com/practice/utils/DateUtil.java
@@ -0,0 +1,48 @@
+package com.practice.utils;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * This is for JDK 8
+ * 
+ */
+public class DateUtil {
+
+	public static final String uuuuMM = "uuuuMM";
+
+	public static YearMonth getAdjusterMonth(String date, long adjuster) {
+		YearMonth localDate = YearMonth.parse(date, DateTimeFormatter.ofPattern(uuuuMM));
+		return localDate.plusMonths(adjuster);
+	}
+
+	public static String toUUUUMM(LocalDate localDate) {
+		return localDate.format(DateTimeFormatter.ofPattern(uuuuMM));
+	}
+	
+	public static String toUUUUMM(YearMonth yearAndMonth) {
+		return yearAndMonth.format(DateTimeFormatter.ofPattern(uuuuMM));
+	}
+
+	/**
+	 * 201401 ---> 201312
+	 * 
+	 * @param uuuuMM
+	 * @return
+	 */
+	public static String getBeforeMonth(String date) {
+		return toUUUUMM(getAdjusterMonth(date, -1));
+	}
+
+	/**
+	 * 201312 ---> 201401
+	 * 
+	 * @param uuuuMM
+	 * @return
+	 */
+	public static String getAfterMonth(String date) {
+		return toUUUUMM(getAdjusterMonth(date, 1));
+	}
+
+}
---
+++ src/com/practice/utils/ReflectUtil.java
@@ -0,0 +1,23 @@
+package com.practice.utils;
+
+public class ReflectUtil {
+	public static void setField(Object object, String fieldName, Object value) {
+		try {
+			java.lang.reflect.Field field = object.getClass().getDeclaredField(fieldName);
+			field.setAccessible(true);
+			field.set(object, value);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void setSuperField(Object object, String fieldName, Object value) {
+		try {
+			java.lang.reflect.Field field = object.getClass().getSuperclass().getDeclaredField(fieldName);
+			field.setAccessible(true);
+			field.set(object, value);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
