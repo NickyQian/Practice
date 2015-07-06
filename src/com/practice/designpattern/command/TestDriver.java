5313f6dbd9d2625529ffaa9dcea1077d73b6468c
Nicky <nicky_qianlei@163.com>
2015-07-06 15:04:15 +0800

    Observer Pattern using Java util

--- src/com/practice/designpattern/command/TestDriver.java
+++ src/com/practice/designpattern/command/TestDriver.java
@@ -7,7 +7,6 @@ import com.practice.designpattern.command.homeapplications.Light;
  * 将“请求”封装成对象，以便使用不同的请求、
  * 队列或者日志来参数化其他对象。命令模式也
  * 支持可撤销的操作。
- * @author qianlei
  *
  */
 public class TestDriver {
---
+++ src/com/practice/designpattern/observer/javaObserver/HumidityForecast.java
@@ -0,0 +1,20 @@
+package com.practice.designpattern.observer.javaObserver;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class HumidityForecast implements Observer {
+
+	public HumidityForecast(WeatherDataUsingJavaUtil weatherData) {
+		weatherData.addObserver(this);
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		WeatherDataUsingJavaUtil weather = (WeatherDataUsingJavaUtil) o;
+		System.out.println("========================");
+		System.out.println("Humidity Forecast: " + weather.getHumidity());
+		System.out.println("Notice: " + arg);
+	}
+
+}
