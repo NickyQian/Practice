7771125bfe86152b1208d9354248a8aa52681b0d
Nicky <nicky_qianlei@163.com>
2015-09-01 14:26:32 +0800

    TIJ: IO

---
+++ src/com/practice/tij/io/filewalker/DirList.java
@@ -0,0 +1,35 @@
+package com.practice.tij.io.filewalker;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public class DirList {
+	public static FilenameFilter filter(final String regex) {
+		return new FilenameFilter() {
+			private Pattern pattern = Pattern.compile(regex);
+
+			@Override
+			public boolean accept(File dir, String name) {
+				return pattern.matcher(name).matches();
+			}
+		};
+	}
+
+	public static void main(String[] args) {
+		File path = new File(".");
+		String regex = ".*";
+		// listFiles return File Objects array
+		File[] list = path.listFiles(filter(regex));
+		for (File file : list) {
+			System.out.println(file.getAbsolutePath());
+		}
+		System.out.println("===");
+		// list return file/directory names
+		String[] filePaths = path.list();
+		Arrays.sort(filePaths, String.CASE_INSENSITIVE_ORDER);
+		for (String s : filePaths)
+			System.out.println(s);
+	}
+}
