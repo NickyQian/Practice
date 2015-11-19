25ce3826fdeb57af8c337ff8447ddb11d1eb607a
Nick <nicky_qianlei@163.com>
2015-11-19 09:07:10 +0800

    modify the crawler, writing content to file.

--- src/com/practice/crawl/WebContentCrawler.java
+++ src/com/practice/crawl/WebContentCrawler.java
@@ -1,7 +1,12 @@
 package com.practice.crawl;
 
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.URL;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -15,10 +20,12 @@ import com.google.common.collect.Maps;
 
 public class WebContentCrawler {
 
-	static PrintStream printer = System.out;
-	//static PrintWriter printer = new PrintWriter(System.out);
+	static PrintWriter printer;
+
+	static String ZEROS = "0000000000";
 
 	static String baseUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2013/";
+
 	public static Map<String, String> provinces = Maps.newLinkedHashMap();
 
 	public static void generateProvince() throws IOException {
@@ -45,18 +52,12 @@ public class WebContentCrawler {
 						Elements a = e.getElementsByTag("a");
 						if (a.size() > 0) {
 							printIndents(count);
-							printer.print(a.get(0).attr("href"));
+							// printer.print(a.get(0).attr("href"));
 							printer.print("\t");
 							printer.print(a.get(0).html());
 							printer.print("\t");
 							printer.println(a.get(1).html());
 							printName(currentUrl, a.get(0).attr("href"));
-						} else {
-							printIndents(count);
-							Elements td = e.getElementsByTag("td");
-							printer.print(td.get(0).text());
-							printer.print("\t");
-							printer.println(td.get(1).text());
 						}
 					}
 				}
@@ -65,17 +66,24 @@ public class WebContentCrawler {
 
 	}
 
-	private static Document getConnectedToRemoteServer(String currentUrl, Document docLv1) {
+	public static String readHtml(String myurl) {
+		StringBuffer sb = new StringBuffer("");
+		URL url;
 		try {
-			docLv1 = Jsoup.connect(currentUrl).get();
-		} catch (IOException e) {
-			try {
-				docLv1 = Jsoup.connect(currentUrl).post();
-			} catch (IOException e1) {
-				printer.println("try again failed.");
-				e1.printStackTrace();
+			url = new URL(myurl);
+			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream(), "gb2312"));
+			String s = "";
+			while ((s = br.readLine()) != null) {
+				sb.append(s + "\r\n");
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+		return sb.toString();
+	}
+
+	private static Document getConnectedToRemoteServer(String currentUrl, Document docLv1) {
+		docLv1 = Jsoup.parse(readHtml(currentUrl), "utf-8");
 		return docLv1;
 	}
 
@@ -104,13 +112,24 @@ public class WebContentCrawler {
 	}
 
 	public static void main(String[] args) throws IOException, InterruptedException {
+		printer = new PrintWriter(new OutputStreamWriter(new FileOutputStream("d://location.txt"), "UTF-8"));
+		// writer = new PrintWriter(System.out);
+		long startTime = System.currentTimeMillis();
 		generateProvince();
 		for (Entry<String, String> entry : provinces.entrySet()) {
-			printer.print(entry.getKey());
+			int index = entry.getKey().lastIndexOf(".html");
+			System.out.println("正在处理 " + entry.getValue() + "....");
+			long start = System.currentTimeMillis();
+			printer.print(entry.getKey().substring(0, index) + ZEROS);
 			printer.print("\t");
 			printer.println(entry.getValue());
 			printName(baseUrl, entry.getKey());
-		}
+			long end = System.currentTimeMillis();
 
+			System.out.println("正在处理 " + entry.getValue() + "结束。耗时" + ((end - start) / 1000) + "秒。");
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("处理完毕，耗时 " + ((endTime - startTime) / 1000) / 60 + "分钟。");
+		printer.close();
 	}
 }
